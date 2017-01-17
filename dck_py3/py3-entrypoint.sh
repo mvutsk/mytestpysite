a81bb28eb3f8d16fedbcac2544a43d41b44ecde2
mike <m.varaxin@gmail.com>
2017-01-17 16:17:28 +0100

    1

--- dck_py3/py3-entrypoint.sh
+++ dck_py3/py3-entrypoint.sh
@@ -4,13 +4,13 @@ set -e
 initsite() {
   echo "Initializing site for starting."
   source /data/site/tenv/bin/activate
-  cd /data/site/tenv/wiki20
+  cd /data/site/tenv/tblog
   pip3 install -e .
   pip3 install -r requirements.txt
   #python3 setup.py develop
-  echo "Starting Circus"
+  #echo "Starting Circus"
   #circusd --daemon /data/site/mywiki34/wiki20/circus.ini
-  circusd circus.ini
+  #circusd circus.ini
 }
 
 initsite_db() {
@@ -18,20 +18,20 @@ initsite_db() {
   cd /data/site
   virtualenv tenv
   source /data/site/tenv/bin/activate
-  mv -v /data/site/wiki20 /data/site/tenv/
-  cd /data/site/tenv/wiki20/
+  mv -v /data/site/tblog /data/site/tenv/
+  cd /data/site/tenv/tblog/
   pip3 install -e .
   pip3 install -r requirements.txt
-  python3 setup.py develop
-  gearbox setup-app  
-  echo "Init completed at `date`" > /data/site/tenv/wiki20/.initcomplete
+  #python3 setup.py develop
+  #gearbox setup-app  
+  echo "Init completed at `date`" > /data/site/tenv/tblog/.initcomplete
   echo "Init setup completed, one user created:
     user: manager
     password: managepass"
 }
 
 if [[ "$1" == "start" ]]; then
-    if [[ -f /data/site/tenv/wiki20/.initcomplete  ]]; then
+    if [[ -f /data/site/tenv/tblog/.initcomplete  ]]; then
         initsite
       else
         echo "Site is not initialized, pls check and make init_db."
@@ -39,14 +39,13 @@ if [[ "$1" == "start" ]]; then
     fi
 	
 elif [[ "$1" == "init_db" ]]; then
-   if [[ -f /data/site/tenv/wiki20/.initcomplete  ]]; then
+   if [[ -f /data/site/tenv/tblog/.initcomplete  ]]; then
        echo "Initialization of site already completed. Nothing to do here."
-     elif [[ -f /data/site/wiki20/setup.py ]]; then
-	      # && [[ ! -f /data/site/tenv/wiki20/.initcomplete ]]; then
+     elif [[ -f /data/site/tblog/setup.py ]]; then
        initsite_db
      else
-       echo "Could not make init setup of site, because could not find /data/site/tenv/wiki20/setup.py:"
-       ls -la /data/site/wiki20/setup.py
+       echo "Could not make init setup of site, because could not find /data/site/tenv/tblog/setup.py:"
+       ls -la /data/site/tblog/setup.py
        exit 1
    fi
    
@@ -55,5 +54,3 @@ else
     echo "args_other: $@"
     exec "$@"
 fi
-
-#exec "$@"
